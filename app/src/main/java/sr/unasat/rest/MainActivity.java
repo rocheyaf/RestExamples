279557b370c914e436b0dbf045ea345658622625
Visitor <roche_yaf@yahoo.com>
2019-02-02 16:27:54 +0100

    changes

--- app/src/main/java/sr/unasat/rest/MainActivity.java
+++ app/src/main/java/sr/unasat/rest/MainActivity.java
@@ -1,8 +1,14 @@
 package sr.unasat.rest;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,6 +17,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scrounger.countrycurrencypicker.library.Buttons.CountryCurrencyButton;
+import com.scrounger.countrycurrencypicker.library.Country;
+import com.scrounger.countrycurrencypicker.library.Currency;
+import com.scrounger.countrycurrencypicker.library.Listener.CountryCurrencyPickerListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,13 +32,40 @@ import sr.unasat.rest.dto.CountryDto;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
-
+    private NotificationHelper notificationHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.text);
         getCountryData();
+        notificationHelper = new NotificationHelper(this);
+
+
+        CountryCurrencyButton spinner2 = (CountryCurrencyButton) findViewById(R.id.spinner2);
+        spinner2.setOnClickListener(new CountryCurrencyPickerListener() {
+            @Override
+            public void onSelectCountry(Country country) {
+                if (country.getCurrency() == null) {
+                    Toast.makeText(MainActivity.this,
+                            String.format("name: %s\ncode: %s", country.getName(), country.getCode())
+                            , Toast.LENGTH_SHORT).show();
+                            notificationHelper.createNotification("Title","nMessage");
+
+
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            String.format("name: %s\ncurrencySymbol: %s", country.getName(), country.getCurrency().getSymbol())
+                            , Toast.LENGTH_SHORT).show();
+                    notificationHelper.createNotification(country.getName(),country.getCode());
+                }
+            }
+
+            @Override
+            public void onSelectCurrency(Currency currency) {
+
+            }
+        });
     }
 
     private void getCountryData() {
---
+++ app/src/main/java/sr/unasat/rest/SomeOtherActivity.java
@@ -0,0 +1,4 @@
+package sr.unasat.rest;
+
+class SomeOtherActivity {
+}
